d027c81c60142fcd7618c523811c2053b8515ea9
UtsavSharma2141 <n01392141@humbermail.ca>
2021-09-27 09:50:59 -0400

    Changed the icon of the app. Added french language support. Added Alert Dialog whether user wants to stay on app or exit.

--- OneHome/src/main/java/ca/thecollective/it/onehome/MainActivity.java
+++ OneHome/src/main/java/ca/thecollective/it/onehome/MainActivity.java
@@ -5,7 +5,9 @@
 package ca.thecollective.it.onehome;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AlertDialog;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,5 +16,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(false);
+        builder.setTitle("Confirm Your Exit!!");
+        builder.setMessage(R.string.exit_dialouge);
+        builder.setIcon(R.drawable.ic_action_exit);
+        builder.setPositiveButton(R.string.exit_yes, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finishAffinity();
+                System.exit(0);
+            }
+        });
+        builder.setNegativeButton(R.string.exit_no, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
     }
 }
