b757f4c77d40a36318709800801098fac856d1bc
DhruvDave1657 <n01401657@humbermail.ca>
2021-10-07 14:55:46 -0400

    Created splash screen.

---
+++ OneHome/src/main/java/ca/thecollective/it/onehome/SplashScreen.java
@@ -0,0 +1,33 @@
+/* Utsav Sharma N01392141 Section A*/
+/* Dhruv Dave N01401657 Section A
+/* Sukhvir Brar N01395820 Section A
+* Pratheep Chandrakumar N01376948 Section A*/
+package ca.thecollective.it.onehome;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class SplashScreen extends AppCompatActivity {
+   @Override
+   protected void onCreate(Bundle savedInstanceState){
+       super.onCreate(savedInstanceState);
+
+       Intent intent = new Intent(this, MainActivity.class);
+       startActivity(intent);
+       finish();
+
+   }
+        }
