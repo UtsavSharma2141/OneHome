575400cc0e0eeed43ba67ca55f338c50a4c28dff
UtsavSharma2141 <n01392141@humbermail.ca>
2021-11-08 22:58:21 -0500

    Added live temperature values in the temperature fragment from database

--- OneHome/src/main/java/ca/thecollective/it/onehome/TemperatureFragment.java
+++ OneHome/src/main/java/ca/thecollective/it/onehome/TemperatureFragment.java
@@ -4,20 +4,75 @@
 * Pratheep Chandrakumar N01376948 Section A*/
 package ca.thecollective.it.onehome;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
 public class TemperatureFragment extends Fragment {
+
+    private TextView temperature,humidity;
+    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    private DatabaseReference mrootReference = firebaseDatabase.getReference();
+    private DatabaseReference mchildReference = mrootReference.child("message");
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_temperature,container,false);
+
+    }
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        temperature = view.findViewById(R.id.live_temp);
+        temperature.setText("Hello");
+        humidity = view.findViewById(R.id.live_humidity);
+
+
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        mchildReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String message = snapshot.getValue(String.class);
+                temperature.setText(message);
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
