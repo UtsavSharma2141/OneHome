b626e30c524ab1a753b195fc9365ffc272c58d92
UtsavSharma2141 <n01392141@humbermail.ca>
2021-12-05 17:35:10 -0500

    Added Espresso unit test class.

---
+++ OneHome/src/androidTest/java/ca/thecollective/it/onehome/MainActivityInstrumentedTest.java
@@ -0,0 +1,27 @@
+package ca.thecollective.it.onehome;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityInstrumentedTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityScenarioRule = new ActivityScenarioRule<>(MainActivity.class);
+
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
