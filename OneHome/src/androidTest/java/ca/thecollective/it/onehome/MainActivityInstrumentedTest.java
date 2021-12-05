05816aeaff9a11014c9f8a1d282d6fcc66e2638f
UtsavSharma2141 <n01392141@humbermail.ca>
2021-12-05 18:10:14 -0500

    Added one test case in MainActivityInstrumentedTest.java unit test class.

--- OneHome/src/androidTest/java/ca/thecollective/it/onehome/MainActivityInstrumentedTest.java
+++ OneHome/src/androidTest/java/ca/thecollective/it/onehome/MainActivityInstrumentedTest.java
@@ -6,8 +6,17 @@ import androidx.test.ext.junit.runners.AndroidJUnit4;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withInputType;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static java.util.EnumSet.allOf;
+import static java.util.regex.Pattern.matches;
+import static org.hamcrest.Matchers.containsString;
 import static org.junit.Assert.*;
 
 @RunWith(AndroidJUnit4.class)
@@ -23,5 +32,11 @@ public class MainActivityInstrumentedTest {
 
     @After
     public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void ButtonIsClicked(){
+        onView(withId(R.id.review_us))
+                .perform(click());
     }
 }
