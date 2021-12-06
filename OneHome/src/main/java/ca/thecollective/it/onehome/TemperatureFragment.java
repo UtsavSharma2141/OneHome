429c36a02811266a222ba0b34d0dfbc3d7ed4393
UtsavSharma2141 <n01392141@humbermail.ca>
2021-12-06 16:06:01 -0500

    added notification when user set the maximum temperature and humidity.

--- OneHome/src/main/java/ca/thecollective/it/onehome/TemperatureFragment.java
+++ OneHome/src/main/java/ca/thecollective/it/onehome/TemperatureFragment.java
@@ -5,6 +5,12 @@
 package ca.thecollective.it.onehome;
 
 import android.annotation.SuppressLint;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,6 +26,7 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.core.app.NotificationCompat;
 import androidx.fragment.app.Fragment;
 
 import com.google.firebase.database.ChildEventListener;
@@ -33,10 +40,13 @@ import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
+import static androidx.core.content.ContextCompat.getSystemService;
+
 public class TemperatureFragment extends Fragment {
 
-    TextView temperature,humidity,max_temp,max_humidity;
+    TextView temperature, humidity, max_temp, max_humidity;
     SeekBar temp_seekbar, humidity_seekbar;
+    private final String CHANNEL_ID = "Notification";
     Switch temp_switch, humidity_switch;
     private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
     private DatabaseReference mrootReference = firebaseDatabase.getReference();
@@ -47,11 +57,10 @@ public class TemperatureFragment extends Fragment {
     private DatabaseReference mchildReference2 = mrootReference.child("message2");
 
 
-
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_temperature,container,false);
+        return inflater.inflate(R.layout.fragment_temperature, container, false);
 
 
     }
@@ -62,21 +71,22 @@ public class TemperatureFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         temperature = view.findViewById(R.id.live_temp);
         humidity = view.findViewById(R.id.live_humidity);
-        max_temp= (TextView) view.findViewById(R.id.max_temp);
-        max_humidity= (TextView) view.findViewById(R.id.max_humidity);
+        max_temp = (TextView) view.findViewById(R.id.max_temp);
+        max_humidity = (TextView) view.findViewById(R.id.max_humidity);
         temp_seekbar = (SeekBar) view.findViewById(R.id.temp_seekbar);
-        humidity_seekbar= (SeekBar) view.findViewById(R.id.humidity_seekbar);
+        humidity_seekbar = (SeekBar) view.findViewById(R.id.humidity_seekbar);
         temp_switch = (Switch) view.findViewById(R.id.temp_switch);
         humidity_switch = (Switch) view.findViewById(R.id.humidity_switch);
 
-        max_temp.setText(getString(R.string.set_max_temperature)+ temp_seekbar.getProgress() + "/"+temp_seekbar.getMax());
+        max_temp.setText(getString(R.string.set_max_temperature) + temp_seekbar.getProgress() + "/" + temp_seekbar.getMax());
         temp_seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
 
             int progress_value;
+
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 progress_value = progress;
-                max_temp.setText(getString(R.string.set_max_temperature)+ progress + "/"+temp_seekbar.getMax());
+                max_temp.setText(getString(R.string.set_max_temperature) + progress + "/" + temp_seekbar.getMax());
 
             }
 
@@ -87,19 +97,20 @@ public class TemperatureFragment extends Fragment {
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-                max_temp.setText(getString(R.string.set_max_temperature)+ progress_value + "/"+temp_seekbar.getMax());
+                max_temp.setText(getString(R.string.set_max_temperature) + progress_value + "/" + temp_seekbar.getMax());
 
             }
         });
 
-        max_humidity.setText(getString(R.string.set_max_humidity)+ humidity_seekbar.getProgress() + "/"+humidity_seekbar.getMax());
+        max_humidity.setText(getString(R.string.set_max_humidity) + humidity_seekbar.getProgress() + "/" + humidity_seekbar.getMax());
         humidity_seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
 
             int progress_value2;
+
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 progress_value2 = progress;
-                max_humidity.setText(getString(R.string.set_max_humidity)+ progress + "/"+humidity_seekbar.getMax());
+                max_humidity.setText(getString(R.string.set_max_humidity) + progress + "/" + humidity_seekbar.getMax());
 
             }
 
@@ -110,7 +121,7 @@ public class TemperatureFragment extends Fragment {
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-                max_humidity.setText(getString(R.string.set_max_humidity)+ progress_value2 + "/"+humidity_seekbar.getMax());
+                max_humidity.setText(getString(R.string.set_max_humidity) + progress_value2 + "/" + humidity_seekbar.getMax());
 
             }
         });
@@ -121,9 +132,9 @@ public class TemperatureFragment extends Fragment {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
 
                 if (isChecked) {
+                    addNotificationTemperature();
 
                     Toast.makeText(getActivity(), getResources().getString(R.string.switch_max_temp), Toast.LENGTH_SHORT).show();
-
 
 
                 } else {
@@ -136,10 +147,11 @@ public class TemperatureFragment extends Fragment {
         humidity_switch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
                 if (isChecked) {
+                    addNotificationHumidity();
 
                     Toast.makeText(getActivity(), getResources().getString(R.string.switch_max_humidity), Toast.LENGTH_SHORT).show();
-
 
 
                 } else {
@@ -148,7 +160,6 @@ public class TemperatureFragment extends Fragment {
                 }
             }
         });
-
 
 
     }
@@ -161,7 +172,7 @@ public class TemperatureFragment extends Fragment {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 Integer message = snapshot.getValue(Integer.class);
-                temperature.setText( message+ "°C");
+                temperature.setText(message + "°C");
 
 
             }
@@ -177,7 +188,7 @@ public class TemperatureFragment extends Fragment {
             public void onDataChange(@NonNull DataSnapshot snapshot) {
 
                 Integer message2 = snapshot.getValue(Integer.class);
-                humidity.setText( message2 + "%");
+                humidity.setText(message2 + "%");
 
             }
 
@@ -186,6 +197,74 @@ public class TemperatureFragment extends Fragment {
 
             }
         });
+
+    }
+
+    private void addNotificationTemperature(){
+        CreateNotificationChannelTemperature();
+        NotificationCompat.Builder builder= new NotificationCompat.Builder(getActivity(),CHANNEL_ID)
+                .setSmallIcon(R.mipmap.ic_launcher_round)
+                .setContentTitle(getString(R.string.max_temp_notification));
+
+        Intent notificationIntent = new Intent(getActivity(), TemperatureFragment.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(getActivity(), 0,notificationIntent,PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(contentIntent);
+
+        NotificationManager manager = ( NotificationManager ) getActivity().getSystemService( getActivity().NOTIFICATION_SERVICE );
+        manager.notify(0,builder.build());
+
+
+
+
+    }
+
+    private void addNotificationHumidity(){
+        CreateNotificationChannelHumidity();
+        NotificationCompat.Builder builder= new NotificationCompat.Builder(getActivity(),CHANNEL_ID)
+                .setSmallIcon(R.mipmap.ic_launcher_round)
+                .setContentTitle(getString(R.string.max_humidity_notification));
+
+        Intent notificationIntent = new Intent(getActivity(), TemperatureFragment.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(getActivity(), 0,notificationIntent,PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(contentIntent);
+
+        NotificationManager manager = ( NotificationManager ) getActivity().getSystemService( getActivity().NOTIFICATION_SERVICE );
+        manager.notify(0,builder.build());
+
+
+
+
+    }
+
+    private void CreateNotificationChannelTemperature() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "You have successfully set the maximum temperature limit.";
+            String description = "Maximum Temperature has been Set.";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+
+            NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ID, name, importance);
+            notificationChannel.setDescription(description);
+            NotificationManager notificationManager = ( NotificationManager ) getActivity().getSystemService( getActivity().NOTIFICATION_SERVICE );
+            notificationManager.createNotificationChannel(notificationChannel);
+
+        }
+
+    }
+
+    private void CreateNotificationChannelHumidity() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "You have successfully set the maximum humidity limit.";
+            String description = "Maximum humidity has been Set.";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+
+            NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ID, name, importance);
+            notificationChannel.setDescription(description);
+            NotificationManager notificationManager = ( NotificationManager ) getActivity().getSystemService( getActivity().NOTIFICATION_SERVICE );
+            notificationManager.createNotificationChannel(notificationChannel);
+
+        }
 
     }
 }
