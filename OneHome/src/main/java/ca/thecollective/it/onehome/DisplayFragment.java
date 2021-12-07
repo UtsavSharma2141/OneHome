651b1bc0460af63aed07afc96eef3df977d52bf6
PratheepChandrakumar6848 <n01376948@humbermail.ca>
2021-12-07 11:05:40 -0500

    Refactored DisplayFragment.java code and removed all comments

--- OneHome/src/main/java/ca/thecollective/it/onehome/DisplayFragment.java
+++ OneHome/src/main/java/ca/thecollective/it/onehome/DisplayFragment.java
@@ -55,15 +55,13 @@ public class DisplayFragment extends Fragment {
         EnterButton = (Button)view.findViewById(R.id.EnterButton);
         Text = (EditText)view.findViewById(R.id.edit);
 
-        //set base state of switches and buttons
+
         switch1.setEnabled(false);
         switch2.setEnabled(false);
         switch3.setEnabled(false);
         switch4.setEnabled(false);
         EnterButton.setEnabled(false);
 
-
-        //onclick for undo snackbar button
         mOnClickListener= new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -71,49 +69,40 @@ public class DisplayFragment extends Fragment {
             }
         };
 
-        //checkbox code
+
         checkbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked){
-                    //enable switches
                     switch1.setEnabled(true);
                     switch2.setEnabled(true);
                     switch3.setEnabled(true);
                     switch4.setEnabled(true);
 
-                    //show snackbar
+
                     Snackbar snackbar = Snackbar
                             .make(getActivity().findViewById(android.R.id.content),"LCD Display ON",Snackbar.LENGTH_LONG)
                             .setAction("undo", mOnClickListener);
                             snackbar.show();
-                }else{
-                    //disable switches
+                } else {
                     switch1.setEnabled(false);
                     switch2.setEnabled(false);
                     switch3.setEnabled(false);
                     switch4.setEnabled(false);
-
                 }
-
             }
         });
 
 
 
-        //switches code
         switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
-                    // If the switch button is on
-                    // Show the switch button checked status as toast message
                     Toast.makeText(getActivity(),
                             "Temperature Display is On", Toast.LENGTH_SHORT).show();
                     firebaseDatabase.getReference().child("LCD").child("Status").setValue(1);
                 } else {
-                    // If the switch button is off
-                    // Show the switch button checked status as toast message
                     Toast.makeText(getActivity(),
                             "Temperature Display is Off", Toast.LENGTH_SHORT).show();
                     firebaseDatabase.getReference().child("LCD").child("Status").setValue(0);
@@ -125,14 +114,10 @@ public class DisplayFragment extends Fragment {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
-                    // If the switch button is on
-                    // Show the switch button checked status as toast message
                     Toast.makeText(getActivity(),
                             "Motion Display is On", Toast.LENGTH_SHORT).show();
                     firebaseDatabase.getReference().child("LCD").child("Motion").setValue(1);
                 } else {
-                    // If the switch button is off
-                    // Show the switch button checked status as toast message
                     Toast.makeText(getActivity(),
                             "Motion Display is Off", Toast.LENGTH_SHORT).show();
                     firebaseDatabase.getReference().child("LCD").child("Motion").setValue(0);
@@ -144,14 +129,10 @@ public class DisplayFragment extends Fragment {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
-                    // If the switch button is on
-                    // Show the switch button checked status as toast message
                     Toast.makeText(getActivity(),
                             "Curtain Status On", Toast.LENGTH_SHORT).show();
                     firebaseDatabase.getReference().child("LCD").child("Curtain").setValue(1);
                 } else {
-                    // If the switch button is off
-                    // Show the switch button checked status as toast message
                     Toast.makeText(getActivity(),
                             "Curtain Status Off", Toast.LENGTH_SHORT).show();
                     firebaseDatabase.getReference().child("LCD").child("Curtain").setValue(0);
@@ -163,15 +144,11 @@ public class DisplayFragment extends Fragment {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
-                    // If the switch button is on
-                    // Show the switch button checked status as toast message
                     Toast.makeText(getActivity(),
                             "Custom Text Display On", Toast.LENGTH_SHORT).show();
                     EnterButton.setEnabled(true);
                     firebaseDatabase.getReference().child("LCD").child("Message").setValue(1);
                 } else {
-                    // If the switch button is off
-                    // Show the switch button checked status as toast message
                     Toast.makeText(getActivity(),
                             "Custom Text Display Off", Toast.LENGTH_SHORT).show();
                     EnterButton.setEnabled(false);
@@ -180,22 +157,18 @@ public class DisplayFragment extends Fragment {
             }
         });
 
-        //enterbutton code
         EnterButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String Message = Text.getText().toString();
                 if(Text.getText().toString().isEmpty()){
                     Toast.makeText(getActivity(), "Field is Empty", Toast.LENGTH_LONG).show();
-                }else {
-                    //if textbox is not empty
+                } else {
                     Toast.makeText(getActivity(), "Text: " + Message + "\n is sent to display", Toast.LENGTH_LONG).show();
                     firebaseDatabase.getReference().child("Text").push().setValue(Message);
                 }
             }
         });
-
         return view;
-
     }
 }
